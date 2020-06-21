b57beefd993942fd9c76be669b25b65768b22cf2
David Elster <davidandtheweb@gmail.com>
2020-06-20 22:37:56 -0400

    Tailwind Macro Fix.

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,42 +2,50 @@ module.exports = {
   siteMetadata: {
     title: `Red Drop (Sh2)`,
     description: `Red Drop is a Ship 2 alliance on PSO2 NA.`,
-    author: `@delsterdev`,
+    author: `@delsterdev`
   },
   plugins: [
+    // SEO
     `gatsby-plugin-react-helmet`,
+    // GraphQL Source: Images
     {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
-        path: `${__dirname}/src/images`,
-      },
+        path: `${__dirname}/src/images`
+      }
     },
+    // GraphQL Source: Pages
     {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `pages`,
-        path: `${__dirname}/src/pages/`,
-      },
+        path: `${__dirname}/src/pages/`
+      }
     },
+    // MDX Parser
     {
       resolve: `gatsby-plugin-mdx`,
       options: {
         defaultLayouts: {
-          default: require.resolve("./src/components/layout.js"),
+          default: require.resolve('./src/components/layout.js')
         },
         gatsbyRemarkPlugins: [
           {
             resolve: `gatsby-remark-images`,
             options: {
-              maxWidth: 1200,
-            },
-          },
-        ],
-      },
+              maxWidth: 1200
+            }
+          }
+        ]
+      }
     },
+    // Styling
+    `gatsby-plugin-styled-components`,
+    // Image Optimization
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
+    // PWA Manifest
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
@@ -47,10 +55,10 @@ module.exports = {
         background_color: `#770000`,
         theme_color: `#770000`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`,
-      },
+        icon: `src/images/gatsby-icon.png`
+      }
     },
     // Progressive Web App + Offline functionality
-    `gatsby-plugin-offline`,
-  ],
+    `gatsby-plugin-offline`
+  ]
 }
